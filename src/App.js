5c528804cebf4d4649781f78a2c7246eafdfe98d
Hyrum Van Slyke <hyrumevanslyke@gmail.com>
2023-09-20 15:54:39 -0400

    Added error page

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/Home";
 import ProductsPage from "./pages/Products";
 import Root from "./pages/Root";
-
+import ErrorPage from "./pages/ErrorPage";
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
+    errorElement: <ErrorPage />,
     children: [
       { path: "/", element: <HomePage /> },
       { path: "/products", element: <ProductsPage /> },
