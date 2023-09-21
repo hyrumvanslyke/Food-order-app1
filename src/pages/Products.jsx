dcd4965cca997c7edcd4dc7587adc59860f5276a
Hyrum Van Slyke <hyrumevanslyke@gmail.com>
2023-09-21 11:53:53 -0400

    Finished router segment

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,13 +1,23 @@
-function ProductsPage(){
-    return (
-        <>
-        <h1>The Products Page</h1>
-        <ul>
-            <li>Product 1</li>
-            <li>Product 2</li>
-            <li>Product 3</li>
-        </ul>
-        </>
-    )
+import { Link } from "react-router-dom";
+
+const PRODUCTS = [
+  { id: "p1", title: "product1" },
+  { id: "p2", title: "product2" },
+  { id: "p3", title: "product3" },
+];
+
+function ProductsPage() {
+  return (
+    <>
+      <h1>The Products Page</h1>
+      <ul>
+        {PRODUCTS.map((prod) => (
+          <li key={prod.id}>
+            <Link to={`/products/${prod.id}`}>{prod.id}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
-export default ProductsPage
+export default ProductsPage;
